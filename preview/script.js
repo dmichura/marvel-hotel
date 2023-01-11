3fbc94e6196b36338dddf810330cf4f0d33ef067
dmichura <dejwidcode@gmail.com>
2023-01-11 07:55:28 +0100

    New look page

--- preview/script.js
+++ preview/script.js
@@ -1,22 +1,17 @@
 const root = document.querySelector(":root");
-/* Hamburger */
-class Hamburger {
-  status = false;
-  timer = null;
-  constructor(hamburger, menu) {
-    this.element = hamburger;
-    this.menu = menu;
-    this.setTransition(1);
-    this.update();
-  }
 
+class Hamburger {
+  constructor(hamburger, menu) {
+    // this.element = hamburger;
+    // this.menu = menu;
+  }
   setTransition(classN) {
     const list = this.element.querySelectorAll("span");
+    // console.log(classN);
     for (let i = 0; i < list.length; i++) {
-      list[i].className = classN;
+      list[i].className = `hamburger-bar ${classN}`;
     }
   }
-
   toggle(force) {
     if (this.timer) return false;
     this.status = force == null ? !this.status : force;
@@ -40,30 +35,83 @@ class Hamburger {
     }
     this.setTransition(
       this.status === false
-        ? "transition-hamburger-close"
-        : "transition-hamburger-open"
+        ? "hamburger-bar-transition-fadeOut"
+        : "hamburger-bar-transition-fadeIn"
     );
     return this.status;
   }
   getElement() {
     return this.element;
   }
-
-  update() {
-    // const h = document.querySelector(".header").offsetHeight;
-    // console.log(h);
-    // root.style.setProperty("--nav-height", h);
-  }
 }
 
-/* Application */
+class Preloader {
+  timer = null;
+  status = false;
+  constructor(element) {
+    this.element = element;
+    this.loadingText = element.querySelector(".preloader-logo-text");
+    this.bar = element.querySelector(".preloader-bar");
+    this.show();
+  }
+
+  changeLoadingText() {
+    let text = this.loadingText.innerText;
+    switch (text) {
+      case "Ładowanie":
+        text += ".";
+        break;
+      case "Ładowanie.":
+        text += ".";
+        break;
+      case "Ładowanie..":
+        text += ".";
+        break;
+      default:
+        text = "Ładowanie";
+        break;
+    }
+    this.loadingText.innerText = text;
+  }
+
+  setPercentage(percentage) {
+    if (percentage >= 0 && percentage <= 100) {
+      this.bar.style.width = `${percentage}%`;
+    }
+  }
+
+  show() {
+    if (this.status === false) {
+      if (this.timer != null) clearInterval(this.timer);
+      this.timer = setInterval(() => {
+        this.changeLoadingText();
+      }, 250);
+      this.setPercentage(0);
+      this.status = true;
+    }
+  }
+
+  hide() {
+    if (this.status) {
+      if (this.timer != null) clearInterval(this.timer);
+      this.timer = null;
+      this.status = false;
+      this.element.classList.toggle("hide", true);
+    }
+  }
+}
 
 class Application {
   constructor() {
     this.hamburger = new Hamburger(
-      document.querySelector(".hamburger-container"),
-      document.querySelector(".nav-wrapper")
+      document.querySelector(".hamburger"),
+      document.querySelector("nav")
     );
+    this.preloader = new Preloader(document.querySelector(".preloader"));
+    // setInterval(() => {
+    //   this.percentage += 5;
+    //   this.preloader.setPercentage(this.percentage);
+    // }, 200);
     // All handlers
     addEventListener("click", (event) => {
       this.clickHandler(event);
@@ -71,6 +119,19 @@ class Application {
     addEventListener("resize", (event) => {
       this.resizeHandler(event);
     });
+    addEventListener("load", (event) => {
+      this.load(event);
+    });
+  }
+
+  load(e) {
+    this.preloader.setPercentage(50);
+    setTimeout(() => {
+      this.preloader.setPercentage(100);
+      setTimeout(() => {
+        this.preloader.hide();
+      }, 100);
+    }, 500);
   }
 
   clickHandler(e) {
@@ -86,7 +147,7 @@ class Application {
   }
 
   resizeHandler(e) {
-    // this.hamburger.toggle(false);
+    this.hamburger.toggle(false);
     // this.hamburger.update();
     return false;
   }
