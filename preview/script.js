33d409621ee43db9c5f9efb721a6655fb983e328
dmichura <dejwidcode@gmail.com>
2023-02-07 03:05:05 +0100

    Added video background & typingText

--- preview/script.js
+++ preview/script.js
@@ -1,53 +1,79 @@
+const header_logo = document.querySelector("#header-logo");
+console.log(header_logo);
+
+class TypingText {
+  text = [];
+  curr = 0;
+  element = null;
+  constructor(e, text) {
+    e.innerText = "";
+    this.element = e;
+    this.curr = 0;
+    this.text = text;
+    this.addLetter();
+  }
+
+  addLetter() {
+    setTimeout(() => {
+      this.element.innerText +=
+        this.text[this.curr][this.element.innerText.length];
+      if (this.element.innerText.length < this.text[this.curr].length) {
+        requestAnimationFrame(() => {
+          this.addLetter();
+        });
+      } else {
+        setTimeout(() => {
+          this.removeLetter();
+        }, 1200);
+      }
+    }, 125);
+  }
+
+  removeLetter() {
+    setTimeout(() => {
+      this.element.innerText = this.element.innerText.substring(
+        0,
+        this.element.innerText.length - 1
+      );
+      if (this.element.innerText.length > 0) {
+        requestAnimationFrame(() => {
+          this.removeLetter();
+        });
+      } else {
+        setTimeout(() => {
+          this.nextText();
+          this.addLetter();
+        }, 1200);
+      }
+    }, 125);
+  }
+
+  nextText() {
+    if (this.curr < this.text.length - 1) {
+      this.curr++;
+    } else {
+      this.curr = 0;
+    }
+
+    console.log(`Curr: ${this.curr}`);
+  }
+}
+
 class Application {
-  data = {};
-  ui = {};
   constructor() {
-    // All handlers
-    this.ui.ball = document.querySelector(".ball");
-    addEventListener("click", (e) => {
-      this.clickHandler(e);
+    this.logoText = new TypingText(header_logo, [
+      "Marvel",
+      "Wypoczynek",
+      "Bługaria",
+      "Bezpieczeństwo",
+    ]);
+    document.addEventListener("DOMContentLoaded", () => {
+      console.log("loaded");
+      this.loadHandler();
     });
-
-    this.ui.navItems = document.querySelectorAll(".nav-item");
-    if (this.ui.navItems.length > 0) {
-      this.ui.navItems.forEach((element, index) => {
-        if (index == 0)
-          this.ui.ball.style.left = `${
-            element.offsetLeft + element.clientWidth / 2
-          }px`;
-        element.addEventListener("mouseenter", (e) => {
-          this.mouseEnterHandler(e);
-        });
-        element.addEventListener("mouseout", (e) => {
-          this.mouseOutHandler(e);
-        });
-      });
-    }
   }
 
-  clickHandler(e) {}
-
-  mouseEnterHandler(e) {
-    if (e.target) {
-      const { target } = e;
-      if (target.classList.contains("nav-item")) {
-        this.ui.ball.style.left = `${
-          target.offsetLeft + target.clientWidth / 2
-        }px`;
-      }
-    }
-  }
-
-  mouseOutHandler(e) {
-    if (e.target) {
-      const target = this.ui.navItems[0];
-      if (target.classList.contains("nav-item")) {
-        this.ui.ball.style.left = `${
-          target.offsetLeft + target.clientWidth / 2
-        }px`;
-      }
-    }
-  }
+  loadHandler() {}
 }
 
 new Application();
