fe8d9dcd0f66d4fb977e7e6990e85a3cdfce56c7
Dawid Michura <47542137+dmichura@users.noreply.github.com>
2023-02-17 06:35:38 +0100

    Added .sql file

---
+++ mysql/marvel-hotel.sql
@@ -0,0 +1,137 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 17 Lut 2023, 06:35
+-- Wersja serwera: 10.4.27-MariaDB
+-- Wersja PHP: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `marvel-hotel`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `account`
+--
+
+CREATE TABLE `account` (
+  `id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `register_time` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `opinion`
+--
+
+CREATE TABLE `opinion` (
+  `id` bigint(20) NOT NULL,
+  `room_id` int(11) NOT NULL,
+  `account_id` int(11) NOT NULL,
+  `content` varchar(5000) NOT NULL,
+  `rate` int(11) NOT NULL,
+  `created_time` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `reservation`
+--
+
+CREATE TABLE `reservation` (
+  `id` bigint(20) NOT NULL,
+  `room_id` int(11) NOT NULL,
+  `account_id` int(11) NOT NULL,
+  `start_time` datetime NOT NULL,
+  `end_time` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `room`
+--
+
+CREATE TABLE `room` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `description` text NOT NULL,
+  `thumbnail_path` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `room`
+--
+
+INSERT INTO `room` (`id`, `name`, `description`, `thumbnail_path`) VALUES
+(1, 'Pokój dla 2 osób', 'Pokój dla dwóch osób', 'room-1bed.webp'),
+(2, 'Czarny pokój dla 2 osób ', 'Pokój dla dwóch osób - 2', 'room-1bed-2.webp'),
+(3, 'Pokój dla 2 osób ', 'Pokój dla dwóch osób, widok na morze', 'room-1bed-3.webp'),
+(4, 'Pokój dla 4 osób', 'Pokój dla czterech osób', 'room-2bed.webp'),
+(5, 'Pokój dla 4 osób 2', 'Pokój dla czterech osób - 2', 'room-2bed-2.webp');
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `account`
+--
+ALTER TABLE `account`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `reservation`
+--
+ALTER TABLE `reservation`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `room`
+--
+ALTER TABLE `room`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT dla zrzuconych tabel
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `account`
+--
+ALTER TABLE `account`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT dla tabeli `reservation`
+--
+ALTER TABLE `reservation`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT dla tabeli `room`
+--
+ALTER TABLE `room`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
